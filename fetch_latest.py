14541c279403b87fb7c324d6020ba831ddf17e02
Honoka55 <1915637207@qq.com>
2025-10-10 18:13:28 +0800

    feat: moniter three channels

--- fetch_latest.py
+++ fetch_latest.py
@@ -1,19 +1,34 @@
 import json
 import feedparser
+from datetime import datetime
 
-
-FEED_URL = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCm0zMBHYwYGzsYnrqE0kk1g'
-feed = feedparser.parse(FEED_URL)
-
-if not feed.entries:
-    raise Exception('No videos found in the feed')
-
-latest = feed.entries[0]
-video_id = latest.yt_videoid
-
-data = {
-    'video_id': video_id
+CHANNELS = {
+    'ZH': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCbELDDHGqMbjsA_4Kyd7vMw',
+    'EN': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCm0zMBHYwYGzsYnrqE0kk1g',
+    'KID': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCQ7Z5kbCT5uv4IyYHVwA3sg'
 }
 
+latest_video = None
+
+for chan, url in CHANNELS.items():
+    feed = feedparser.parse(url)
+    if not feed.entries:
+        continue
+
+    entry = feed.entries[0]
+    published = datetime(*entry.published_parsed[:6])
+
+    if latest_video is None or published > latest_video['published']:
+        latest_video = {
+            'channel': chan,
+            'video_id': entry.yt_videoid,
+            'title': entry.title,
+            'published': published
+        }
+
+if latest_video is None:
+    raise Exception('No videos found in any feed')
+
+latest_video['published'] = latest_video['published'].isoformat()
 with open('latest.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False)
+    json.dump(latest_video, f, ensure_ascii=False)
